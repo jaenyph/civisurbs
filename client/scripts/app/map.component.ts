c49b904eae4ba26a29318121dd5a860c62432f37
jaenyph <jaenyph@pelemail.net>
2017-01-29 21:11:40 +0100

    Add panning to map component for both mouse and touch events
    Inspired from https://www.codeproject.com/articles/355230/html-canvas-a-simple-paint-program-touch-and-mou

--- client/scripts/app/map.component.ts
+++ client/scripts/app/map.component.ts
@@ -17,10 +17,16 @@ export class MapComponent {
     private _game: Phaser.Game;
     private _isoGroup: any;
     private _cursorPos: Phaser.Plugin.Isometric.Point3;
-    private _cursorKeys: Phaser.CursorKeys;
+    private _inputCursorKeys: Phaser.CursorKeys;
+    private _isPanning: boolean;
+    private _panningStartPosition: Phaser.Point;
     private _isometric: Phaser.Plugin.Isometric.Projector;
 
     constructor() {
+
+        this._isPanning = false;
+        this._panningStartPosition = new Phaser.Point(0, 0);
+
         this._game = new Phaser.Game(
             640,
             480,
@@ -66,9 +72,12 @@ export class MapComponent {
 
     private gameCreate() {
 
-        this._cursorKeys = this._game.input.keyboard.createCursorKeys();
         const game = this._game;
         game.world.setBounds(0, 0, 1920, 1920);
+        this._inputCursorKeys = game.input.keyboard.createCursorKeys();
+
+        this.initializePanningEvents();
+
 
         // Create a group for our tiles.
         this._isoGroup = this._game.add.group();
@@ -131,6 +140,139 @@ export class MapComponent {
         }
     }
 
+    private processPanning(game: Phaser.Game, pointerInitialPosition: Phaser.Point, pointerCurrentPosition: Phaser.Point): void {
+        const panningSpeedReducer = 0.077;
+        const deltaPosition = pointerCurrentPosition.subtract(pointerInitialPosition.x, pointerInitialPosition.y);
+        const newCameraPosition = new Phaser.Point(game.camera.x, game.camera.y)
+            .add(deltaPosition.x * panningSpeedReducer, deltaPosition.y * panningSpeedReducer);
+        game.camera.x = newCameraPosition.x;
+        game.camera.y = newCameraPosition.y;
+    }
+
+    private initializePanningEvents() {
+
+        const game = this._game;
+
+        // This will be defined on a TOUCH device such as iPad or Android, etc.
+        const is_touch_device = 'ontouchstart' in document.documentElement;
+
+        if (is_touch_device) {
+            // create a drawer which tracks touch movements
+            const drawer = {
+                isDrawing: false,
+                touchstart: (coors: any) => {
+                    this._isPanning = true;
+                    this._panningStartPosition = new Phaser.Point(coors.x, coors.y);
+                },
+                touchmove: (coors: any) => {
+                    if (this._isPanning) {
+                        this.processPanning(game, this._panningStartPosition, new Phaser.Point(coors.x, coors.y));
+                    }
+                },
+                touchend: (coors: any) => {
+                    if (this._isPanning) {
+                        drawer.touchmove(coors);
+                        this._isPanning = false;
+                    }
+                }
+            };
+
+
+            // create a function to pass touch events and coordinates to drawer
+            const draw = (event: any) => {
+                // get the touch coordinates.  Using the first touch in case of multi-touch
+                const coors = {
+                    x: event.targetTouches[0].pageX,
+                    y: event.targetTouches[0].pageY
+                };
+
+                // Now we need to get the offset of the canvas location
+                let obj: any = this._game.canvas;
+
+                if (obj.offsetParent) {
+                    // Every time we find a new object, we add its offsetLeft and offsetTop to curleft and curtop.
+                    do {
+                        coors.x -= obj.offsetLeft;
+                        coors.y -= obj.offsetTop;
+                    }
+                    // The while loop can be "while (obj = obj.offsetParent)" only, which does return null
+                    // when null is passed back, but that creates a warning in some editors (i.e. VS2010).
+                    while ((obj = obj.offsetParent) != null);
+                }
+
+                // pass the coordinates to the appropriate handler
+                drawer[event.type](coors);
+            }
+
+
+            // attach the touchstart, touchmove, touchend event listeners.
+            const canvas = game.canvas;
+            canvas.addEventListener('touchstart', draw, false);
+            canvas.addEventListener('touchmove', draw, false);
+            canvas.addEventListener('touchend', draw, false);
+
+            // prevent elastic scrolling
+            canvas.addEventListener('touchmove', function (event) {
+                event.preventDefault();
+            }, false);
+        } else {
+
+            // start drawing when the mousedown event fires, and attach handlers to
+            // draw a line to wherever the mouse moves to
+            game.canvas.addEventListener('mousedown', (mouseEvent) => {
+
+                const updatePosition = (nativeEvent: MouseEvent, canvas: HTMLCanvasElement, pointToUpdate?: Phaser.Point): Phaser.Point => {
+                    const result = pointToUpdate || new Phaser.Point();
+                    let x, y;
+                    if (nativeEvent.pageX != undefined && nativeEvent.pageY != undefined) {
+                        x = nativeEvent.pageX;
+                        y = nativeEvent.pageY;
+                    } else {
+                        x = nativeEvent.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                        y = nativeEvent.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+                    }
+
+                    return result.set(x - canvas.offsetLeft, y - canvas.offsetTop);
+                }
+
+                const canvas = mouseEvent.target as HTMLCanvasElement;
+
+
+                let onMouseMove: (nativeEvent: MouseEvent) => void;
+                let onMouseUp: (nativeEvent: MouseEvent) => void;
+                let onMouseOut: (nativeEvent: MouseEvent) => void;
+
+                const terminatePanning = () => {
+                    canvas.removeEventListener('mousemove', onMouseMove, false);
+                    canvas.removeEventListener('mouseup', onMouseUp, false);
+                    canvas.removeEventListener('mouseout', onMouseOut, false);
+                    this._isPanning = false;
+                };
+
+                onMouseMove = (nativeEvent: MouseEvent) => {
+                    if (this._isPanning) {
+                        this.processPanning(game, this._panningStartPosition, updatePosition(nativeEvent, canvas));
+                    }
+                };
+                onMouseUp = (nativeEvent: MouseEvent) => {
+                    terminatePanning();
+                };
+                onMouseOut = (nativeEvent: MouseEvent) => {
+                    this._isPanning = false;
+                    terminatePanning();
+                };
+
+                canvas.addEventListener('mousemove', onMouseMove, false);
+                canvas.addEventListener('mouseup', onMouseUp, false);
+                canvas.addEventListener('mouseout', onMouseOut, false);
+
+                updatePosition(mouseEvent, game.canvas, this._panningStartPosition);
+                this._isPanning = true;
+
+            }, false);
+
+        }
+    }
 
     private spawnTiles() {
         const tiles = [
