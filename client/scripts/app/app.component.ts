69298847cc204880cd5a2e9a17d6614d994e1e51
jaenyph <jaenyph@pelemail.net>
2017-01-30 20:33:16 +0100

    Move app.component template to own html file

--- client/scripts/app/app.component.ts
+++ client/scripts/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>AppComponent : {{status}}</h1><cu-map></cu-map>`
+  moduleId: module.id,
+  templateUrl: './app.component.html'
 })
 export class AppComponent { status = 'loaded'; }
